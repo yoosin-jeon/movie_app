d0bdb35bd6cd63386a52ea523d3bb1e9fff6794f
전유신 <yoosin0513@inforex.co.kr>
2020-05-07 10:01:22 +0900

    Updates

---
+++ precache-manifest.1dd5080fd85958e4fe0a4e9ea4d3af84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba8f910db4accb599ab59a27f25f1de",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "74f8aff1a1d71cd99f07",
+    "url": "/movie_app/static/css/main.a8401d6d.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f8aff1a1d71cd99f07",
+    "url": "/movie_app/static/js/main.78044317.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
